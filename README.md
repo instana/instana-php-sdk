a9b93b4d6ae8e02f9584446eab4c3ad7fa7fe37b
Gordon Oheim <gordon.oheim@instana.com>
2019-09-18 15:25:40 +0200

    initial commit
    covers Instana PHP SDK as of PHP Tracer v1.4.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# Instana PHP SDK stubs
+
+This repo provide stubs for the [Instana PHP SDK][docs]. You can use the stubs for adding autocompletion to your IDE. In addition, the stubs serve as a no-op fallback in case the Instana PHP SDK is unavailable in your code. The stubs provide no other functionality. The actual Instana PHP SDK is bundled with the Instana PHP Tracing extension.
+
+## Installing via Composer
+
+Install Composer in a common location or in your project:
+
+    curl -s http://getcomposer.org/installer | php
+
+Install via composer:
+
+    composer require --dev instana/instana-php-sdk
+
+## Usage
+
+Please see our [official documentation for the Instana PHP Tracer][docs] for a usage example.
+
+ [docs]: https://docs.instana.io/ecosystem/php/#php-sdk
---
+++ instana.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Instana SDK extension stubs
+ *
+ * Example:
+ * <code>
+ * <?php
+ *     $tracer = new Instana\Tracer();
+ *     $span = $tracer->createSpan('foo');
+ *     $span->annotate('function', 'doSomething');
+ *     try {
+ *         doSomething();
+ *     } catch (\Exception $e) {
+ *         $tracer->logException($e);
+ *         $span->markError();
+ *     } finally {
+ *        $span->stop();
+ *     }
+ *</code>
+ *
+ * @link https://docs.instana.io/ecosystem/php/#php-sdk
+ * @package Instana
+ */
+namespace Instana;
+
+/**
+ * Class Instana\Tracer
+ *
+ * @package Instana
+ */
+class Tracer
+{
+    /**
+     * Tracer constructor.
+     */
+    public function __construct(){}
+
+    /**
+     * Creates a new intermediate SDK Span with the name set to $category
+     *
+     * @param string $category
+     * @return Span
+     */
+    public function createSpan($category){}
+
+    /**
+     * Logs an exception
+     *
+     * @param \Exception $e
+     * @return void
+     */
+    public function logException(\Exception $e){}
+
+    /**
+     * Sets the Service Name
+     * 
+     * @param string $serviceName
+     * return void
+     */
+    public static function setServiceName($serviceName){}
+}
+
+/**
+ * Class Instana\Span
+ *
+ * An individual Span in a Trace
+ *
+ * @package Instana
+ */
+class Span
+{
+    /**
+     * Span constructor.
+     *
+     * Creating Spans directly through this constructor will create orphaned Spans that won't show up in a trace.
+     * Use <code>Tracer::createSpan()</code> instead.
+     *
+     * @see Tracer::createSpan()
+     */
+    public function __construct(){}
+
+    /**
+     * Annotates the Span with a key and a value
+     *
+     * Setting the same key multiple times will overwrite any previously set value.
+     *
+     * @param string $key
+     * @param string|int $val
+     * @throws InstanaRuntimeException when Span was already stopped
+     * @throws InstanaRuntimeException when $key is not a string
+     * @throws InstanaRuntimeException when $value is not a string or integer
+     * @return void
+     */
+    public function annotate($key, $val){}
+
+    /**
+     * Marks the Span as erroneous
+     *
+     * @throws InstanaRuntimeException when Span was already stopped
+     * @return void
+     */
+    public function markError(){}
+
+    /**
+     * Closes the Span
+     *
+     * @return void
+     */
+    public function stop(){}
+}
+
+/**
+ * Class InstanaRuntimeException
+ *
+ * @package Instana
+ */
+class InstanaRuntimeException {}
